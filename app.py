a681eddde2cdd713d23f998204b26e4c52598efb
imshawan <shawan.sm049@gmail.com>
2022-07-06 10:33:19 +0530

    New structure v6

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def start_job(filename, message, api_key):
     print(f"Filename: {filename}, \n Total records: {total}, Successful: {successful}, Failed: {failed}")
 
     try:
-        callbck = requests.post('http://broadcast.heyx.io/user/user/broadcast_callback', json={
+        requests.post('http://broadcast.heyx.io/user/user/broadcast_callback', json={
             "result_url": f"/{app.config['PROCESSED_FOLDER']}/processed-{filename}.csv",
             "stats": {
                 "total": total,
@@ -165,8 +165,6 @@ def start_job(filename, message, api_key):
             },
             "name": "broadcaster"
         })
-        print(callbck.status_code)
-        print(callbck.text)
     except:
         pass
     # print(f"Job ended at: {get_timestamp()}")
